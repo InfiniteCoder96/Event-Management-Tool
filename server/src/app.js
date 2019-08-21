53478efc3a7df5860f5568c6542a5fc2d2052b32
InfiniteCoder96 <pasinduvimansa96@gmail.com>
2019-08-21 23:50:08 +0530

    Added a Procfile.

--- server/src/app.js
+++ server/src/app.js
@@ -1,30 +1,34 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const cors = require('cors');
+
+const app = express();
 
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(express.json()); 
-app.use(cors());
+
+
 
 
 require('dotenv').config();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 
 
 const middlewares = require('./middlewares');
 const api = require('./api');
 
-const app = express();
 
 
 
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-type');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.json({
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,9 @@
 const app = require('./app');
 
+app.get("/", (req, res) => {
+  res.send("Welcome to a basic express App");
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   /* eslint-disable no-console */
