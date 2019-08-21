c34e4b1800f2ce6a0c8d6403802f6c0a34463431
InfiniteCoder96 <pasinduvimansa96@gmail.com>
2019-08-21 17:07:01 +0530

    Final touches

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,7 @@ app.use(cors());
 require('dotenv').config();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://guestmap.inofinitylabs.com/");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
